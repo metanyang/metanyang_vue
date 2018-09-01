b6f23b3456e283b8c01d01356ce823b58ae4a01e
먕 <>
2018-09-02 06:03:33 +0900

    shelter

---
+++ src/components/shelter/ShelterSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="mainWrap">
+      <sheltersection-step1 v-show="this.$store.getters.getShelterStep === 1"></sheltersection-step1>
+    </div>
+  </section>
+</template>
+
+<script>
+import ShelterSectionStep1 from '@/components/shelter/ShelterSectionStep1'
+import ShelterSectionStep2 from '@/components/shelter/ShelterSectionStep2'
+import ShelterSectionStep3 from '@/components/shelter/ShelterSectionStep3'
+import { SHELTER_INIT_STEP } from '@/store/actions'
+
+export default {
+  components: {
+    'sheltersection-step1': ShelterSectionStep1,
+    'sheltersection-step2': ShelterSectionStep2,
+    'sheltersection-step3': ShelterSectionStep3
+  },
+  created () {
+    if (this.$store.getters.getShelterStep > 1) {
+      this.$store.commit(SHELTER_INIT_STEP)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/shelter/ShelterSectionStepCommon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="title">{{ title }}</div>
+    <div class="sub"><slot></slot> </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '받은후원'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
